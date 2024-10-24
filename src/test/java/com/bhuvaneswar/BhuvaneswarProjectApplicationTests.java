a8ee0f9d101548b42dd4b61fb35af41630665ae3
bhuvaneswar <bhuvaneswarr412@gmail.com.com>
2024-10-24 11:22:25 +0530

    Initial commit

---
+++ src/test/java/com/bhuvaneswar/BhuvaneswarProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhuvaneswar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhuvaneswarProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
